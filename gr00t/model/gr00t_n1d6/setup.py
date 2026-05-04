d1d7974b45cc71533841f58b0551ffe038d924e2
Ubuntu <ubuntu@ip-172-31-26-152.ec2.internal>
2026-05-04 17:33:23 +0000

    fix rtc with ft

--- gr00t/model/gr00t_n1d6/setup.py
+++ gr00t/model/gr00t_n1d6/setup.py
@@ -75,6 +75,7 @@ class Gr00tN1d6Pipeline(ModelPipeline):
                 tune_vlln=self.config.model.tune_vlln,
                 state_dropout_prob=self.config.model.state_dropout_prob,
                 backbone_trainable_params_fp32=self.config.model.backbone_trainable_params_fp32,
+                rtc_max_delay=self.config.model.rtc_max_delay,
                 transformers_loading_kwargs=self.transformers_loading_kwargs,
                 output_loading_info=True,
                 **self.transformers_loading_kwargs,
--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -17,7 +17,7 @@ CUDA_VISIBLE_DEVICES=0 python \
     --embodiment-tag NEW_EMBODIMENT \
     --modality-config-path examples/ALOHA/custom_aloha_config.py \
     --num-gpus $NUM_GPUS \
-    --output-dir checkpoints/260423_gr00t_aloha_cube_handover_rtc12_80Hz_v4 \
+    --output-dir checkpoints/260423_gr00t_aloha_cube_handover_rtc12_80Hz_v5 \
     --save-total-limit 5 \
     --save-steps 2000 \
     --max-steps 50000 \
@@ -25,4 +25,4 @@ CUDA_VISIBLE_DEVICES=0 python \
     --global-batch-size 32 \
     --color-jitter-params brightness 0.3 contrast 0.4 saturation 0.5 hue 0.08 \
     --dataloader-num-workers 4 \
-    --rtc_max_delay 12
+    --rtc-max-delay 12
